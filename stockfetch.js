d54cdb7522f444c207a39533b038fd7a05293043
Miroslaw Kucharzyk <kucharzyk.miroslaw@gmail.com>
2016-07-22 11:26:32 +0200

    Change collaborators interface to become async

--- stockfetch.js
+++ stockfetch.js
@@ -1,11 +1,12 @@
 module.exports = stockfetch;
 
 function stockfetch(file, tickerReader, priceGetter) {
-    var tickers = tickerReader(file);
-    var prices = priceGetter(tickers);
+    return tickerReader(file).then(function(tickers) {
+        return priceGetter(tickers);
+    });
 
     // console.log("Result: " + prices);
-    return [tickers, prices];
+
 }
 
 
--- tests/stockfetchTest.js
+++ tests/stockfetchTest.js
@@ -1,22 +1,26 @@
 var assert = require('assert');
-var stockfetch = require('../stockfetch');
 
 describe('End to end test', function() {
     it('should work', function () {
+        var stockfetch = require('../stockfetch');
 
         var fileReader = function(filename) {
-            return ['foo', 'bar'];
+            assert(filename, './test');
+            return Promise.resolve(['foo', 'bar']);
         };
+
         var tickerPricesGetter = function(tickers) {
-            return tickers.map(function (cb, index) {
+            var prices = tickers.map(function (cb, index) {
                 return index;
             });
+
+            assert.deepEqual(tickers, ['foo', 'bar']);
+            return Promise.resolve(prices);
         };
 
-        var output = stockfetch('test', fileReader, tickerPricesGetter);
-        tickers = output[0]; prices = output[1];
 
-        assert.deepEqual(['foo', 'bar'], tickers);
-        assert.deepEqual([0, 1], prices);
+        return stockfetch('./test', fileReader, tickerPricesGetter).then(function(prices) {
+            assert.deepEqual(prices, [0,1]);
+        });
     });
 });
