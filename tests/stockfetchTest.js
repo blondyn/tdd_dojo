467d743f0c75dedbdf1dc0a0907e9fa8792a8a54
Miroslaw Kucharzyk <kucharzyk.miroslaw@gmail.com>
2016-07-22 11:03:16 +0200

    Add first test !!!11111

---
+++ tests/stockfetchTest.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var stockfetch = require('../stockfetch');
+
+describe('End to end test', function() {
+    it('should work', function () {
+
+        var fileReader = function(filename) {
+            return ['foo', 'bar'];
+        };
+        var tickerPricesGetter = function(tickers) {
+            return tickers.map(function (cb, index) {
+                return index;
+            });
+        };
+
+        var output = stockfetch('test', fileReader, tickerPricesGetter);
+        tickers = output[0]; prices = output[1];
+
+        assert.deepEqual(['foo', 'bar'], tickers);
+    });
+});
